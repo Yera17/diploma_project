a8203b49318c8b8fbdb5a32cc9f06a1692816cbd
Yera17 <erko.salaly@gmail.com>
2025-02-01 19:09:41 +0100

    initial commit

---
+++ clothing_store/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from my_app.views import index, contact
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('contact/', contact, name='contact'),
+    path('admin/', admin.site.urls),
+]
