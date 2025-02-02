d2e68edb7421b2869359ea2a0f47aead50531fc6
Yera17 <erko.salaly@gmail.com>
2025-02-02 19:23:51 +0100

    database homepage

--- my_app/views.py
+++ my_app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from products.models import Product, Category
 # Create your views here.
 def index(request):
-    return render(request, 'my_app/index.html')
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    return render(request, 'my_app/index.html', {
+        'categories': categories,
+        'products': products,
+    })
 
 def contact(request):
     return render(request, 'my_app/contact.html')
--- products/models.py
+++ products/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, related_name="products",on_delete=models.CASCADE)
+    price = models.FloatField()
+    description = models.TextField()
+    brand = models.CharField(max_length=255)
+    department = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    img_url = models.URLField()
+    asin = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name="images",on_delete=models.CASCADE)
+    url = models.URLField()
+
+class ProductVariation(models.Model):
+    product = models.ForeignKey(Product, related_name="variations",on_delete=models.CASCADE)
+    color = models.CharField(max_length=255)
+    asin = models.CharField(max_length=255)
+
+class ProductSize(models.Model):
+    product = models.ForeignKey(Product, related_name="sizes",on_delete=models.CASCADE)
+    value = models.CharField(max_length=255)
